50b3fe46f93fc1db6e5aea4a48efa7cb5495990b
Kris McGinnes <kmcginnes@me.com>
2015-05-01 11:31:57 -0500

    Initial commit

---
+++ PoC.CastleWindsorGenericDecorators/OpenGenericDecoratorTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PoC.CastleWindsorGenericDecorators
+{
+    [TestFixture]
+    public class OpenGenericDecoratorTests
+    {
+        [Test]
+        public void ShouldDecorateConcreteWithOpenGeneric()
+        {
+            var container = new WindsorContainer();
+            container.Register(
+                Component.For(typeof(IHandle<>)).ImplementedBy(typeof(LoggingHandler<>)),
+                Component.For<IHandle<PlayEvent>>().ImplementedBy<PlayEventHandler>()
+            );
+
+            var handlers = container.ResolveAll<IHandle<PlayEvent>>();
+
+            for (int i = 0; i < handlers.Length; i++)
+            {
+                var handler = handlers[i];
+                Console.WriteLine(string.Format("## Handler {0}: {1}", i+1, handler.GetType().Name));
+                handler.Handle(new PlayEvent());
+                Console.WriteLine();
+            }
+
+            handlers.Should().HaveCount(1);
+        }
+    }
+
+    public interface IHandle<in T>
+    {
+        void Handle(T message);
+    }
+
+    public class LoggingHandler<T> : IHandle<T>
+    {
+        readonly IHandle<T> _decorated;
+
+        public LoggingHandler(IHandle<T> decorated)
+        {
+            _decorated = decorated;
+        }
+
+        public void Handle(T message)
+        {
+            Console.WriteLine("LoggingHandler.Handle :: Before");
+            _decorated.Handle(message);
+            Console.WriteLine("LoggingHandler.Handle :: After");
+        }
+    }
+
+    public class PlayEventHandler : IHandle<PlayEvent>
+    {
+        public void Handle(PlayEvent message)
+        {
+            Console.WriteLine("PlayEventHandler.Handle");
+        }
+    }
+
+    public class PlayEvent { }
+}
